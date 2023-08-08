a5b8c4e10d3929fe9f4143225590bda15b1fa66a
Dylan Madhani <dmadbusiness@gmail.com>
2023-08-08 15:22:31 -0500

    Fixed pad issue where pads would not play sounds after leaving and coming back to index. Pad will play sounds when clicked. Devise is working but needs additional testing. Event listeners multiply after leaving and coming back to index.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,27 +4,22 @@
 Rails.application.config.assets.version = "1.0"
 
 #Hacksaw way of precompilation. Automate this later
-Rails.application.config.assets.precompile += %w(preset-1/kick_2.wav)
-Rails.application.config.assets.precompile += %w(preset-1/kick_4.wav)
-Rails.application.config.assets.precompile += %w(preset-1/clap_8.wav)
-Rails.application.config.assets.precompile += %w(preset-1/clap_14.wav)
-Rails.application.config.assets.precompile += %w(preset-1/snare_1.wav)
-Rails.application.config.assets.precompile += %w(preset-1/snare_13.wav)
-Rails.application.config.assets.precompile += %w(preset-1/cymbal_1.wav)
-Rails.application.config.assets.precompile += %w(preset-1/cymbal_4.wav)
-Rails.application.config.assets.precompile += %w(preset-1/hihat_1.wav)
-Rails.application.config.assets.precompile += %w(preset-1/hihat_3.wav)
-Rails.application.config.assets.precompile += %w(preset-1/hihat_4.wav)
-Rails.application.config.assets.precompile += %w(preset-1/hihat_12.wav)
-Rails.application.config.assets.precompile += %w(preset-1/tom_2.wav)
-Rails.application.config.assets.precompile += %w(preset-1/tom_3.wav)
-Rails.application.config.assets.precompile += %w(preset-1/tom_6.wav)
-Rails.application.config.assets.precompile += %w(preset-1/tom_13.wav)
-
-
-
-
-
+#Rails.application.config.assets.precompile += %w(preset-1/kick_2.wav)
+#Rails.application.config.assets.precompile += %w(preset-1/kick_4.wav)
+#Rails.application.config.assets.precompile += %w(preset-1/clap_8.wav)
+#Rails.application.config.assets.precompile += %w(preset-1/clap_14.wav)
+#Rails.application.config.assets.precompile += %w(preset-1/snare_1.wav)
+#Rails.application.config.assets.precompile += %w(preset-1/snare_13.wav)
+#Rails.application.config.assets.precompile += %w(preset-1/cymbal_1.wav)
+#Rails.application.config.assets.precompile += %w(preset-1/cymbal_4.wav)
+#Rails.application.config.assets.precompile += %w(preset-1/hihat_1.wav)
+#Rails.application.config.assets.precompile += %w(preset-1/hihat_3.wav)
+#Rails.application.config.assets.precompile += %w(preset-1/hihat_4.wav)
+#Rails.application.config.assets.precompile += %w(preset-1/hihat_12.wav)
+#Rails.application.config.assets.precompile += %w(preset-1/tom_2.wav)
+#Rails.application.config.assets.precompile += %w(preset-1/tom_3.wav)
+#Rails.application.config.assets.precompile += %w(preset-1/tom_6.wav)
+#Rails.application.config.assets.precompile += %w(preset-1/tom_13.wav)
 #Rails.application.config.assets.precompile += %w(preset-1/*.wav)
 
 # Add additional assets to the asset load path.
