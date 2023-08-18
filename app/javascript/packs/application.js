50b9f954d1d2e7dc70831e7732011aa941fb336a
Dylan Madhani <dmadbusiness@gmail.com>
2023-08-18 17:52:27 -0500

    Added final preset to application.js and assets folder Final preset is added and functional.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,8 @@
-var preset_plate = ["kick_1","kick_2","clap_1","clap_2","snare_1","snare_2","cymbal_1","cymbal_2","hihat_1","hihat_2","hihat_3","hihat_4","tom_1","tom_2","tom_3","tom_4"]; 
-var preset_num = [1,2,3];
+// Default preset files for percussion
+var preset_plate = ["kick_1","kick_2","clap_1","clap_2","snare_1","snare_2","cymbal_1","cymbal_2","hihat_1","hihat_2","hihat_3","hihat_4","tom_1","tom_2","tom_3","tom_4"];
+// Default preset files for keys
+var preset_keys = ["c6","b5","asharp5","a5","gsharp5","g5","fsharp5","f5","e5","dsharp5","d5","csharp5","c5","b4","asharp4","a4"];  
+var preset_num = [1,2,3,4,5];
 var preset_position = 0;
 
 function insertPartial(assetPath, buttonLabel) {
@@ -17,16 +20,25 @@ function insertPartial(assetPath, buttonLabel) {
 }
 
 function updatePads() {
+
+    console.log('preset label change');
     var presetLabel = document.getElementById('presetLabel');
     presetLabel.innerHTML = 'Preset ' + preset_num[preset_position];
+
     // Call the function with different values
     for (var i = 0; i < 16; i++){
-
-        var asset_path = '/assets/preset-' + preset_num[preset_position] + '/' + preset_plate[i];
-        var pad_label = 'Pad ' + (i+1);
-        insertPartial(asset_path, pad_label);
+        if (preset_num[preset_position] < 4) {
+            var asset_path = '/assets/preset-' + preset_num[preset_position] + '/' + preset_plate[i];
+            var pad_label = 'Pad ' + (i+1);
+            insertPartial(asset_path, pad_label);
+        } else {
+            var asset_path = '/assets/preset-' + preset_num[preset_position] + '/' + preset_keys[i];
+            var pad_label = 'Pad ' + (i+1);
+            insertPartial(asset_path, pad_label);
+        }
 
     }
+
 }
 
 function buttonAudio() {
@@ -47,20 +59,12 @@ function buttonAudio() {
 document.addEventListener('turbo:load', function() {
     console.log('turbo:load event fired');       
 
-    
-    console.log('update');
-    updatePads();
-    console.log('audio');
-    buttonAudio();
-
     document.body.addEventListener('click', function(event) {
         if (event.target.id === 'nextButton') {
     
             console.log('Next Preset button clicked');
             preset_position++;
-            if (preset_position < 0) {
-                preset_position = preset_num.length - 1;
-            } else if (preset_position > preset_num.length - 1) {
+            if (preset_position > preset_num.length - 1) {
                 preset_position = 0;
             }
             console.log('next: ' + preset_position);
@@ -71,14 +75,18 @@ document.addEventListener('turbo:load', function() {
             preset_position--;
             if (preset_position < 0) {
                 preset_position = preset_num.length - 1;
-            } else if (preset_position > preset_num.length - 1) {
-                preset_position = 0;
-            }
+            } 
             console.log('last: ' + preset_position);
             
         } 
             
     });
+    
+    console.log('update');
+    updatePads();
+    console.log('audio');
+    buttonAudio();
+    
 
 });
 
@@ -92,5 +100,5 @@ document.getElementById("back-link").addEventListener("click", function() {
   });
 
 
-
+  
 
