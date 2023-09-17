5f5fc20942bca07bccb5cec7de0798cdd2df57dc
Dylan Madhani <madhani.1@osu.edu>
2023-09-17 18:23:35 -0400

    Initial styling Added color and basic stylingh elements.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,33 +1,5 @@
-var form = document.querySelector('#your-form-id');
 const playButtons = document.querySelectorAll('.play-button');
 
-function removePartial(container) {
-
-    // Insert the partial HTML into the container
-    if (container != null) {
-        container.innerHTML = "";
-    }
-}
-
-function buttonAudio() {
-    console.log("first listen");
-    const playButtons = document.querySelectorAll('.play-button');
-
-    playButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-        console.log("second listen");
-        const audioPlayer = button.nextElementSibling;
-        audioPlayer.play();
-        audioPlayer.currentTime = 0;
-        });
-    });
-}
-
-document.addEventListener('turbo:load', function() {
-
-
-
-});
 
 
 
